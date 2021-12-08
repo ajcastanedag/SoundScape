ec00afcfb37862e3278284e7ca5ecf7e2e126e74
unknown <antonio.castanedag@gmail.com>
2021-12-08 15:04:09 +0100

    tst

--- 1.Main/1.Main.R
+++ 1.Main/1.Main.R
@@ -28,7 +28,6 @@ WuLoc <- st_read( paste0(Main_Fo, "\\2.SampleData\\SoundSegmentation\\Coordiante
 # Calculate 
 EntropyWz <- Entropy(UrbAtl_Pol,"class_2018",500,"Hex",1)
 
-
 #############################################################################################
 #buffer locations and get areas
 
@@ -60,6 +59,51 @@ ggplot(CircleArea, aes(x="", y=class_2018, fill=class_2018)) +
 # summarize UA classes
 
 UrbanAtlas_TST <- UrbAtl_Pol[,c(1,5)] 
+
+UrbanAtlas_TST <- UrbAtl_Pol[,c(1,5)] 
+
+UrbanAtlas_TST <- st_set_geometry(UrbanAtlas_TST, NULL)
+
+Residential <- c("Continuous urban fabric (S.L. : > 80%)",
+                 "Discontinuous dense urban fabric (S.L. : 50% -  80%)",
+                 "Discontinuous medium density urban fabric (S.L. : 30% - 50%)",
+                 "Discontinuous low density urban fabric (S.L. : 10% - 30%)",
+                 "Discontinuous very low density urban fabric (S.L. : < 10%)")
+
+Industrial <- c("Industrial, commercial, public, military and private units", "Land without current use")
+
+
+Traffic_Infrastructures <- c("Railways and associated land","Fast transit roads and associated land",
+                             "Other roads and associated land","Port areas","Airports")
+
+Non_Residential <- c("Mineral extraction and dump sites","Construction sites", "Isolated structures")
+
+Recreational <- c("Green urban areas", "Sports and leisure facilities", "Forests", "Herbaceous vegetation associations (natural grassland, moors...)")
+
+Others <- c("Water", "Arable land (annual crops)", "Pastures")
+
+
+for(i in 1:length(UrbAtl_Pol$ITEM2012)){
+  if(UrbanAtlas_TST$ITEM2012[i] %in% Residential){UrbanAtlas_TST$ITEM2012[i] <- "Residential"}
+  if(UrbanAtlas_TST$ITEM2012[i] %in% Industrial){UrbanAtlas_TST$ITEM2012[i] <- "Industrial"}
+  if(UrbanAtlas_TST$ITEM2012[i] %in% Traffic_Infrastructures){UrbanAtlas_TST$ITEM2012[i] <- "Traffic Infrastructures"}
+  if(UrbanAtlas_TST$ITEM2012[i] %in% Non_Residential){UrbanAtlas_TST$ITEM2012[i] <- "Non Residential"}
+  if(UrbanAtlas_TST$ITEM2012[i] %in% Recreational){UrbanAtlas_TST$ITEM2012[i] <- "Recreational"}
+  if(UrbanAtlas_TST$ITEM2012[i] %in% Others){UrbanAtlas_TST$ITEM2012[i] <- "Others"}
+  
+}
+
+ggboxplot(UrbAtl_Pol, x = "ITEM2012", y = "Mean", 
+          color = "ITEM2012", palette = c("#2bb8cf","#68cfa3","#a6eac9","#9653d5","#47418a","#4a7fdf","#43c3f1","#fbee9c","#c30b8d"),
+          order = c("Residential","Industrial","Traffic Infrastructures","Non Residential","Recreational",
+                    "Others"),
+          ylab = "Mean", xlab = "UA Class")+
+  theme(axis.text.x = element_text(angle = 90))
+
+ggline(UrbanAtlas_TST, x = "ITEM2012", y = "Mean", 
+       add = c("mean_se", "jitter"), 
+       order = c("Residential","Industrial","Traffic Infrastructures","Non Residential","Recreational",
+
 
 UrbanAtlas_TST <- st_set_geometry(UrbanAtlas_TST, NULL)
 
