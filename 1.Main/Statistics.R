391ece20a048982893e76453bf5f24dbb4d7b3c8
Doutrespace <nils.karges@stud-mail.uni-wuerzburg.de>
2022-01-16 21:18:51 +0100

    tests

--- 1.Main/Statistics.R
+++ 1.Main/Statistics.R
@@ -2,7 +2,8 @@
 ResultsFo <- "C:\\Users\\nilsk\\Desktop\\Soundscape_Git\\SoundScape\\4.Results"
 
 #libs
-library("ggpubr")
+library(ggpubr)
+library(corrplot)
 
 
 #data
@@ -12,6 +13,8 @@ GSSounds <- read.csv2("C:/Users/nilsk/Desktop/Soundscape_Git/SoundScape/4.Result
 HSSounds <- read.csv2("C:/Users/nilsk/Desktop/Soundscape_Git/SoundScape/4.Results/HSSounds.csv")
 TSSounds <- read.csv2("C:/Users/nilsk/Desktop/Soundscape_Git/SoundScape/4.Results/TSSounds.csv")
 
+#test for cor for all var
+GSSounds2 <- read.csv2("C:/Users/nilsk/Desktop/Soundscape_Git/SoundScape/4.Results/GSSounds2.csv")
 
 #pearson
 ggscatter(GSSounds, x = "NDVI", y = "Sound_share", 
@@ -20,8 +23,18 @@ ggscatter(GSSounds, x = "NDVI", y = "Sound_share",
           #xlab = "NDVI", ylab = "Sound share")
 
 
+# correlation for all variables
+round(cor(GSSounds2),
+      digits = 2 # rounded to 2 decimals
+)
 
+# improved correlation matrix
+corrplot(cor(GSSounds2),
+         method = "number",
+         type = "upper" # show only upper side
+)
+
+
+#corr for only two var
 res2 <-cor.test(GSSounds$BD, GSSounds$Sound_share,  method = "spearman")
 res2
-
-
